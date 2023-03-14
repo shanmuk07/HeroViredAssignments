8907dfb3519d0f0682c8c154a40d75ff01a8eb11
MADDIMSETTI S SRINIVASA SATYA SAI GANESH <104714887+shanmuk07@users.noreply.github.com>
2023-03-14 11:12:51 +0530

    PL/SQL

--- pl_sql_loops.sql
+++ pl_sql_loops.sql
@@ -22,3 +22,20 @@ i := i+1;
 end loop;
 end;
 /
+
+
+declare
+i number;
+n number;
+fact number;
+begin
+i := 1;
+fact := 1;
+n := &n;
+for i IN 1..n
+loop
+fact := fact * i;
+end loop;
+dbms_output.put_line(fact);
+end;
+ 15  /
